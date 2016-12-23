c9c1abc9393606675988a629432c1a509490b576
Raimondas Valickas <raimondas@vinted.com>
2016-12-23 15:22:18 +0200

    Update code.js

--- code.js
+++ code.js
@@ -69,9 +69,9 @@ function magic() {
 
     $('.js-category-selector .options-list__content')._rnd().click();
     setTimeout(function() {
-        $('.js-subcategory-select .nav-item').eq(0)._rnd().click();
+        $('.js-subcategory-select .nav-item')._rnd().click();
         setTimeout(function() {
-            $('.js-subcategory-select .nav-item').eq(1)._rnd().click();
+            $('.js-subcategory-select.no-children .nav-item')._rnd().click();
         }, 500);
     }, 500);
 
